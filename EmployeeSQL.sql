13a260d48282960c2d77e653c0ac821c07df5247
Steven Rojano <70564781+sjrojanooo@users.noreply.github.com>
2020-11-14 10:07:52 -0700

    new checkpoint @ task 4

--- EmployeeSQL.sql
+++ EmployeeSQL.sql
@@ -101,26 +101,26 @@ FROM employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1987
 ORDER BY hire_date;
 
---3. List the manager of each department with the following information: 
---department number, department name, the manager's employee number, last name, first name.
+--3. List the manager of each department with the following information:
+-- department number, department name, the manager's employee number, last name, first name.
+SELECT h.Address, p.Name, p.Address as OwnerAddress, i.url FROM house AS h
+INNER JOIN person AS p ON p.personID = h.personID
+INNER JOIN images AS i ON i.personID = p.personID
+GROUP BY h.houseID
 
 SELECT 
-	dept_manager.dept_no,
+	departments.dept_no, 
 	departments.dept_name, 
-	employees.emp_no, 
-	employees.last_name,
-	employees.first_name 
-FROM dept_manager 
-INNER JOIN employees 
-ON employees.emp_no = dept_manager.emp_no
-INNER JOIN departments 
-ON departments.dept_no = dept_manager.dept_no;
+	employees.emp_no,
+	employees.dept_no, 
+	employees.last_name, 
+	employees.first_name
+FROM employees
+INNER JOIN departments ON 
+employees.
+	
+SELECT * FROM dept_manager;
 
---4. List the department of each employee with the following information: 
---employee number, last name, first name, and department name.
-SELECT 
+SELECT * FROM employees;
 
-SELECT * FROM employees; 
-SELECT * FROM departments; 
-SELECT * FROM dept_manager; 
-
+SELECT * FROM departments;
